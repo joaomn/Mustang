275af8075ac194cb2099c85f5d92cada72d4c1d6
joaomn <97440058+joaomn@users.noreply.github.com>
2024-04-16 08:57:57 -0300

    configuracoes iniciais

---
+++ mustang/src/test/java/br/com/mustang/MustangApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mustang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
